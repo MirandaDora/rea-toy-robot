fe88f80a4d941f8f2b0054f9bfa2f9eb6035a89d
mirandali <lisirui19881020@gmail.com>
2021-02-10 01:47:58 +1100

    linting

--- modules/commands/command.ts
+++ modules/commands/command.ts
@@ -3,45 +3,45 @@ export abstract class Command {
   y: number = 0;
   facing: string = 'NORTH'
   name: string
-  constructor(name:string) {
+  constructor(name: string) {
     this.name = name
   }
-  action(x: number, y:number, facing:string){
+  action(x: number, y: number, facing: string) {
     return {
       x: this.x,
       y: this.y,
       facing: this.facing
     }
-}
+  }
 }
 
 export class Place extends Command {
-  constructor(name:string){
+  constructor(name: string) {
     super(name)
   }
-  action(x: number, y:number, facing:string){
-    return {x, y, facing}
+  action(x: number, y: number, facing: string) {
+    return { x, y, facing }
   }
 }
 
 export class Move extends Command {
-  action(x: number, y:number, facing:string){
+  action(x: number, y: number, facing: string) {
     switch (facing) {
       case 'NORTH':
-        y = y-1
+        y = y - 1
         break;
       case 'SOUTH':
-        y = y+1
+        y = y + 1
         break;
       case 'EAST':
-        x = x-1
+        x = x - 1
         break
       case 'WEST':
-        x = x+1
+        x = x + 1
         break
       default:
         break;
     }
-  return{x, y, facing}
+    return { x, y, facing }
   }
 }
